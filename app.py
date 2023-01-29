2f7ad39df8574ec54f6f5ee5f3cf05f853a80eb1
BharathKarkera <94306701+BharathKarkera@users.noreply.github.com>
2023-01-29 12:01:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
-import flask 
+import flask
 import elasticsearch
 import flask_cors
 import json
+import redis
 
 app=flask.Flask(__name__)
 app.config["DEBUG"]=True
 app.config["TOKEN"]="bharathkarkera"
+app.secret_key = 'bharathkarkera'
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 cors = flask_cors.CORS(app, resources={r"/search": {"origins": "http://localhost:5000"}})
 
+redis=redis.Redis(host='localhost',port=6379)
+
 es=elasticsearch.Elasticsearch(hosts=["http://localhost:9200"])
 print(f"Connected to Elastic search cluster: { es.info().body['cluster_name'] }")
 
-MAX_SIZE=10 
+MAX_SIZE=15
 
 @app.route('/')
 def index_path():
-    return flask.render_template("index.html")
+   redis.incr("hits")
+   flask.flash(str(int(redis.get("hits")))+"th hit to the site")
+   return flask.render_template("index.html")
 
+@app.route('/display',methods=["GET"])
+def display_function():
+   redis.incr("hits")
+   return f'Hi This is a test webserver and this is your {int(redis.get("hits"))}th visit to the website'
 
 @app.route('/search',methods=["GET","POST"])
 @flask_cors.cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
@@ -26,10 +36,8 @@ def search_fun():
     query=flask.request.args["q"].lower()
     print(query)
     words=query.split(" ")
-    print(words)
-    
+    print(f"words: {words}")
     #https://www.cleancss.com/python-beautify/
-    
     clauses_list=    [  {
                             "span_multi": {
                                 "match": {
@@ -43,8 +51,8 @@ def search_fun():
                             }
                          }
                          for i in words
-                      ]    
-    
+                      ]
+
     payload={
             "bool": {
                 "must": [{
@@ -54,24 +62,27 @@ def search_fun():
                 }]
             }
     }
-    
-    
+
+
     #127.0.0.1:5000/search?q=bmw%20xl
-    print(payload)
+    print(f"payload is : {payload}")
 
     response= es.search(index="cars", query=payload, size=MAX_SIZE)
-    print(response)
-    
+    print(f"response: {response}")
+
     car_list=[]
     for i in response['hits']['hits']:
         print(i)
         car_list.append(i['_source']['name'])
-    
+
     search_results=tuple([result['_source']['name'] for result in response['hits']['hits']])
-    print(search_results)
+    print(f"json dump: {json.dumps(search_results)}")
 
     #return { "cars": search_results }
+
+
     return json.dumps(search_results)
 
 app.run(host="0.0.0.0")
 
+#sed -e 's/\xc2\xa0/ /g' app.py
